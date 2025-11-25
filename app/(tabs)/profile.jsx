e0b25897019d546b9f4b6e7f6d6977075a6451a9
Ramzan <muhammadramzan3043@gmail.com>
2025-11-25 16:47:09 +0500

    ramzan

--- app/(tabs)/profile.jsx
+++ app/(tabs)/profile.jsx
@@ -1,7 +1,8 @@
 import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import { router } from "expo-router";
 import { useEffect, useState } from "react";
-import { StyleSheet, Text, TouchableOpacity, View, Image } from "react-native";
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import EditProfileModal from "../../components/EditProfileModal";
 import Header from "../../components/header";
 
@@ -28,11 +29,7 @@ export default function Index() {
 
   // 🔥 FIX: Prevent crash while user is null
   if (!user) {
-    return (
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <Text style={{ fontSize: 18, color: "#555" }}>Loading user...</Text>
-      </View>
-    );
+    router.push("login/index");
   }
 
   return (
